105549288624c8b9886624cfb11320753930364c
tkaria <tkaria.info@gmail.com>
2020-10-12 10:26:35 +0900

    add sample

---
+++ Assets/App/Runtime/AttributeTest.cs
@@ -0,0 +1,38 @@
+﻿/*
+  reference:
+    - 実装に関する参考資料をこちらに添付する
+*/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace App.Runtime
+{
+	/// <summary>
+	///  何の仕事を担当するクラスか？
+	/// </summary>
+	public sealed class AttributeTest : MonoBehaviour
+	{
+        #region Field
+
+        [ReorderableList] public List<GameObject> objList;
+
+        #endregion Field
+
+		#region Method
+
+		void Start()
+		{
+
+		}
+
+		void Update()
+		{
+
+		}
+
+		#endregion Method
+	}
+}
